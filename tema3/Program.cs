89a7071fb59bf3f9d2f5929a943b5b373d5e0267
NistorElena-Simona <elena.nistor1@student.usv.ro>
2023-11-18 22:05:59 +0200

    Am adaugat tema 5

---
+++ tema3/Program.cs
@@ -0,0 +1,58 @@
+﻿using ConsoleApp3;
+
+using System;
+using System.Security.Cryptography;
+
+using tema3.Tema2;
+using tema3.Tema3;
+using tema3.Tema4;
+
+namespace tema3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Alegeti proiectul:
+1. Tema2
+2. Tema3
+3. Tema4
+4. Tema5
+
+Introduceti optiunea: ");
+
+            var optiune = Console.ReadLine();
+
+            switch (optiune)
+            {
+                case "1":
+                    using (Tema2Window window = new Tema2Window())
+                    {
+                        window.Run();
+                    }
+                    break;
+                case "2":
+                    using (Tema3Window window = new Tema3Window())
+                    {
+                        window.Run();
+                    }
+                    break;
+                case "3":
+                    using (Tema4Window window = new Tema4Window()) 
+                    {
+                        window.Run();
+                    }
+                    break;
+                    case "4":
+                    using (tema5_window window = new tema5_window())
+                    {
+                        window.Run();
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Optiune necunoscuta");
+                    break;
+            }    
+        }
+    }
+}
---
+++ tema3/Tema4/Camera3D.cs
@@ -0,0 +1,57 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace tema3.Tema4
+{
+    public class Camera3D
+    {
+        private const int VISUAL_EDGE = 40;
+        private Vector3 eye = new Vector3(20, 20, 40);
+        private Vector3 target = new Vector3(0, 0, 0);
+        private Vector3 up = new Vector3(0, 1, 0);
+        private const int MOVEMENT_UNIT = 3;
+
+        //Initialising
+        public void SetCamera()
+        {
+            Matrix4 camera = Matrix4.LookAt(eye, target, up);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref camera);
+        }
+
+        public void RotateRight()
+        {
+            Matrix3 rotationMatrix = Matrix3.CreateRotationY(MathHelper.DegreesToRadians(MOVEMENT_UNIT));
+            eye = Vector3.Transform(eye - target, rotationMatrix) + target;
+            SetCamera();
+        }
+
+        public void RotateLeft()
+        {
+            Matrix3 rotationMatrix = Matrix3.CreateRotationY(MathHelper.DegreesToRadians(-MOVEMENT_UNIT));
+            eye = Vector3.Transform(eye - target, rotationMatrix) + target;
+            SetCamera();
+        }
+
+
+        //Checking camera status
+        public void ControlCamera(MouseState mouse)
+        {
+
+            if (mouse[MouseButton.Right])
+            {
+                this.RotateRight();
+            }
+            else if (mouse[MouseButton.Left])
+            {
+                this.RotateLeft();
+            }
+            else
+            {
+                // do nothing
+            }
+
+        }
+    }
+}
